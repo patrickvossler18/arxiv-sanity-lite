0939ea6f461475b83c6513d710ffcab128b00256
Patrick Vossler <patrick.vossler18@gmail.com>
2022-02-21 13:36:39 -0800

    only get papers every other day

--- fetch_papers.sh
+++ fetch_papers.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-cd /home/ubuntu/arxiv-sanity-lite
-python3 arxiv_daemon.py --num 2000
+if [ $(expr $(date +%s) / 86400 % 2) -eq 0]; then
+    cd /home/ubuntu/arxiv-sanity-lite
+        python3 arxiv_daemon.py --num 2000
 
-if [ $? -eq 0 ]; then
-    echo "New papers detected! Running compute.py"
-    python3 compute.py
-else
-    echo "No new papers were added, skipping feature computation"
+        if [ $? -eq 0 ]; then
+            echo "New papers detected! Running compute.py"
+            python3 compute.py
+        else
+            echo "No new papers were added, skipping feature computation"
+        fi
 fi
